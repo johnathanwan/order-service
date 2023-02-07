d65201892c6a2239c407296452277a7da2291dd8
Johnathan Wan <johnathan.wan@outlook.com>
2023-02-07 15:33:10 +0800

    Remove OderServiceApplicationTests

--- src/test/kotlin/com/polarbookshop/orderservice/OrderServiceApplicationTests.kt
+++
@@ -1,7 +0,0 @@
-package com.polarbookshop.orderservice
-
-import org.junit.jupiter.api.Test
-import org.springframework.boot.test.context.SpringBootTest
-
-@SpringBootTest
-class OrderServiceApplicationTests {}
